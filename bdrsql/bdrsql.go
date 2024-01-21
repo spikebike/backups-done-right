d71e713e2ed121fe31b6811665ec5eb8b482ef4e
Bill Broadley <bill@broadley.org>
2024-01-21 00:20:23 -0700

    Got SQLiteDB created and tables initialized.  Moved db-init to bdrsql.go

--- bdrsql/bdrsql.go
+++ bdrsql/bdrsql.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"syscall"
+	"time"
 )
 import _ "github.com/mattn/go-sqlite3"
 
@@ -16,10 +18,106 @@ var (
 		"create index ctimeindex on files (ctime)",
 		"create index pathindex on dirs (path)",
 	}
+	debug bool
 )
 
-func Init_db(dataBaseName string, newDB bool, debug bool) (db *sql.DB, err error) {
-	if newDB == true {
+func GetSQLID(db *sql.DB, tablename string, field string, value string) (int64, error) {
+	var dirID int64
+
+	dirID = -1
+
+	query := "select id from " + tablename + " where " + field + "= (?)"
+	//	fmt.Printf("query=%s\n", query)
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Printf("GetSQLID: prepare select failed: %s\n", err)
+	}
+	err = stmt.QueryRow(value).Scan(&dirID)
+	if err != nil {
+		if debug == true {
+			log.Printf("GetSQLID: missing %s, error %s\n", value, err)
+		}
+
+		insert := "insert into " + tablename + "(" + field + ") values(?);"
+		stmt, err := db.Prepare(insert)
+		if err != nil {
+			log.Println(err)
+			return -1, err
+		}
+		result, err := stmt.Exec(value)
+		dirID, err = result.LastInsertId()
+	}
+	defer stmt.Close()
+	return dirID, err
+}
+
+func GetSQLFiles(db *sql.DB, dirID int64) map[string]int64 {
+	var fileMap = map[string]int64{}
+	var name string
+	var mtime int64
+
+	stmt, err := db.Prepare("select name,mtime from files where dirID=? and deleted=0")
+	if err != nil {
+		fmt.Printf("GetSQLFiles prepare of select failed: %s\n", err)
+	}
+	defer stmt.Close()
+
+	rows, err := stmt.Query(dirID)
+	if err != nil {
+		fmt.Printf("GetSQLFiles query failed: %s\n", err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		rows.Scan(&name, &mtime)
+		fileMap[name] = mtime
+	}
+	return fileMap
+}
+
+func InsertSQLFile(db *sql.DB, fi os.FileInfo, dirID int64) error {
+	now := time.Now().Unix()
+	e, _ := fi.Sys().(*syscall.Stat_t)
+
+	stmt, err := db.Prepare("insert into files(name,size,mode,gid,uid,ino,dev,mtime,ctime,last_seen,dirID,deleted,do_upload) values(?,?,?,?,?,?,?,?,?,?,?,?,?)")
+	if err != nil {
+		log.Printf("InsertSQL prepare: %s\n", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(fi.Name(), e.Size, e.Mode, e.Gid, e.Uid, e.Ino, e.Dev, e.Mtim.Sec, e.Ctim.Sec, now, dirID, 0, 1)
+	if err != nil {
+		log.Printf("InsertSQL Exec: %s\n", err)
+		return err
+	}
+	return err
+}
+
+func SetSQLSeen(db *sql.DB, fmap map[string]int64, dirID int64) {
+	now := time.Now().Unix()
+	//	tx,_ := db.Begin()
+	update := fmt.Sprintf("update files set last_seen=%d where name=? and dirID=%d and deleted=0 and ctime=?", now, dirID)
+	stmt, _ := db.Prepare(update)
+	defer stmt.Close()
+	for i, _ := range fmap {
+		//		log.Printf("file = %v dirID=%d\n",i,dirID)
+		stmt.Exec(i, fmap[i])
+	}
+	//	tx.Commit()
+}
+
+func SetSQLDeleted(db *sql.DB, now int64) {
+	stmt, err := db.Prepare("update files set deleted=1 where last_seen<?")
+	if err != nil {
+		log.Println(err)
+	}
+	defer stmt.Close()
+
+	stmt.Exec(now)
+}
+
+func Init_db(dataBaseName string, resetDB bool, debug bool) (db *sql.DB, err error) {
+	if resetDB == true {
 		// rm dataBaseName*  (all backups)
 		fps, _ := filepath.Glob(dataBaseName + "*")
 		for _, fp := range fps {
@@ -40,7 +138,7 @@ func Init_db(dataBaseName string, newDB bool, debug bool) (db *sql.DB, err error
 	} else {
 		log.Println("Database opened %s", dataBaseName)
 	}
-	if newDB == true {
+	if resetDB == true {
 		for _, sqli := range clientSQLs {
 			_, err = db.Exec(sqli)
 			if err != nil && debug == true {
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"fmt"
+	"github.com/spf13/viper"
 	"github.com/spikebike/backups-done-right/bdrsql"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 var (
@@ -13,10 +19,129 @@ var (
 	debug bool
 )
 
+func backupDir(db *sql.DB, dirList []string, excludeList []string, dataBaseName string) error {
+	var (
+		fileC       int64
+		backupFileC int64
+		dirC        int64
+		dFile       int64
+		dDir        int64
+	)
+
+	fileC = 0
+	dirC = 0
+	backupFileC = 0
+	dFile = 0
+	dDir = 0
+	start := time.Now().Unix()
+
+	for i, dirname := range dirList {
+		fmt.Printf("%d: %s\n", i+1, dirname)
+		// get dirID of dirname, even if it needs inserted.
+		dirID, err := bdrsql.GetSQLID(db, "dirs", "path", dirname)
+		// get a map for filename -> modified time
+		SQLmap := bdrsql.GetSQLFiles(db, dirID)
+		if debug == true {
+			fmt.Printf("scanning dir %s ", dirname)
+		}
+		d, err := os.Open(dirname)
+		if err != nil {
+			log.Printf("failed to open %s error : %s", dirname, err)
+			os.Exit(1)
+		}
+		fi, err := d.Readdir(-1)
+		if err != nil {
+			log.Printf("directory %s failed with error %s", dirname, err)
+		}
+		Fmap := map[string]int64{}
+		// Iterate over the entire directory
+		dFile = 0
+		dDir = 0
+		for _, f := range fi {
+			if !f.IsDir() {
+				fileC++ //track files per backup
+				dFile++ //trace files per directory
+				// and it's been modified since last backup
+				if f.ModTime().Unix() <= SQLmap[f.Name()] {
+					// log.Printf("NO backup needed for %s \n",f.Name())
+					Fmap[f.Name()] = f.ModTime().Unix()
+				} else {
+					// log.Printf("backup needed for %s \n",f.Name())
+					backupFileC++
+					bdrsql.InsertSQLFile(db, f, dirID)
+				}
+			} else { // is directory
+				dirC++ //track directories per backup
+				dDir++ //track subdirs per directory
+				fullpath := filepath.Join(dirname, f.Name())
+
+				if !checkPath(dirList, excludeList, fullpath) {
+					dirList = append(dirList, fullpath)
+				}
+			}
+		}
+		// All files that we've seen, set last_seen
+		t1 := time.Now().UnixNano()
+		bdrsql.SetSQLSeen(db, Fmap, dirID)
+		if debug == true {
+			t2 := time.Now().UnixNano()
+			fmt.Printf("files=%d dirs=%d duration=%dms\n", dFile, dDir, (t2-t1)/1000000)
+		}
+		i++
+	}
+	// if we have not seen the files since start it must have been deleted.
+	bdrsql.SetSQLDeleted(db, start)
+
+	log.Printf("scanned %d files and %d directories\n", fileC, dirC)
+	log.Printf("%d files scheduled for backup\n", backupFileC)
+
+	return nil
+}
+
+func checkPath(dirArray []string, excludeArray []string, dir string) bool {
+	for _, j := range excludeArray {
+		if strings.Contains(dir, j) {
+			return true
+		}
+	}
+	for _, i := range dirArray {
+		if i == dir {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	var dirList, excludeList []string
+	var err error
 	fmt.Println("Hello, World!")
-	dataBaseName := "fsmeta.sql"
 	debug = true
+
+	viper.SetConfigName("config.json")
+	viper.SetConfigType("json")
+	viper.AddConfigPath(".")
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Config file not found...")
+	} else {
+		name := viper.GetString("name")
+		fmt.Println("Reading values from the config file:", name)
+	}
+
+	err = viper.UnmarshalKey("client.dirList", &dirList)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct %v", err)
+	}
+	dataBaseName := viper.GetString("client.dataBaseName")
+
+	err = viper.UnmarshalKey("client.excludeList", &excludeList)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct %v", err)
+	}
+
+	log.Printf("The database name is: %s\n", dataBaseName)
 
 	db, err := bdrsql.Init_db(dataBaseName, true, debug)
 	if err != nil {
@@ -27,4 +152,6 @@ func main() {
 	if db == nil {
 		log.Printf("missing db")
 	}
+
+	err = backupDir(db, dirList, excludeList, dataBaseName)
 }
