2463546c9ed2cc62dc4613048692110b00711088
Bill Broadley <bill@broadley.org>
2024-01-28 21:51:47 -0700

    added working protobuf tls example

---
+++ src/examples/client-server-tls-proto/client/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/binary"
+	"fmt"
+   "github.com/spikebike/backups-done-right/src/examples/client-server-tls/sum"
+	"google.golang.org/protobuf/proto"
+	"io"
+)
+
+func main() {
+
+	config := &tls.Config{
+		InsecureSkipVerify: true, // uncomment this line for local testing without valid SSL certificates
+		ServerName: "servername",
+	}
+
+	conn, err := tls.Dial("tcp", "localhost:4040", config)
+	if err != nil {
+		fmt.Printf("client: dial: %s", err)
+	}
+
+	if err != nil {
+		fmt.Printf("Failed to connect to server: %v", err)
+	}
+	defer conn.Close()
+
+	numbers := &sum.Numbers{
+		A: 3,
+		B: 5,
+	}
+
+	out, err := proto.Marshal(numbers)
+	if err != nil {
+		fmt.Printf("Failed to encode message: %v", err)
+	}
+
+	var buf [4]byte
+	binary.LittleEndian.PutUint32(buf[0:], uint32(len(out)))
+	conn.Write(buf[0:])
+	conn.Write(out)
+
+	_, err = conn.Read(buf[0:])
+	if err != nil {
+		fmt.Printf("Failed to read response: %v", err)
+	}
+
+	length := binary.LittleEndian.Uint32(buf[0:])
+	data := make([]byte, length)
+
+	_, err = io.ReadFull(conn, data)
+	if err != nil {
+		fmt.Printf("Failed to read response: %v", err)
+	}
+
+	response := &sum.Sum{}
+	if err := proto.Unmarshal(data, response); err != nil {
+		fmt.Printf("Failed to parse response: %v", err)
+	}
+
+	fmt.Printf("The sum is: %d\n", response.Result)
+}
