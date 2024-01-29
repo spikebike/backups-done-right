0ad6b661cf822f57f8fa7ef8a4b82a869365642b
Bill Broadley <bill@broadley.org>
2024-01-28 21:45:00 -0700

    added server and sum service

--- src/examples/client-server-tls/makecert.sh
+++ src/examples/client-server-tls/makecert.sh
@@ -16,3 +16,4 @@ openssl req -new -nodes -x509 -out certs/server.pem -keyout certs/server.key -da
 echo
 echo "make client cert"
 openssl req -new -nodes -x509 -out certs/client.pem -keyout certs/client.key -days 3650 -subj "/C=DE/ST=NRW/L=Earth/O=Random Company/OU=IT/CN=www.random.com/emailAddress=$1"
+
