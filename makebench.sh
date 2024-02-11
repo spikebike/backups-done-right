df91ed959051dbed838d4577a69b1a5bc3c130bc
Bill Broadley <bill@broadley.org>
2024-02-10 20:22:45 -0700

    benchmark update

--- makebench.sh
+++ makebench.sh
@@ -2,6 +2,7 @@
 mkdir -p /tmp/bdr/output
 mkdir -p /tmp/bdr/input
 cd /tmp/bdr/input
+echo "Making 36 files that total 9GB"
 for i in `seq 1 36`; do 
 	echo $i;
 	dd if=/dev/urandom of=$i count=16384 bs=16384; 
